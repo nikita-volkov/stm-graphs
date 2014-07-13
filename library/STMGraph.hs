b815724bd2ede9d61892ac190f7b77d1797b320d
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-07-13 17:49:20 +0400

    Init

---
+++ library/STMGraph.hs
@@ -0,0 +1,11 @@
+-- |
+-- A heterogenous directed mutable graph in STM.
+module STMGraph
+(
+)
+where
+
+import STMGraph.Prelude
+
+
+
---
+++ library/STMGraph/Prelude.hs
@@ -0,0 +1,13 @@
+module STMGraph.Prelude
+( 
+  module Exports,
+)
+where
+
+-- base
+-------------------------
+import BasePrelude as Exports
+
+-- hashable
+-------------------------
+import Data.Hashable as Exports (Hashable(..))
